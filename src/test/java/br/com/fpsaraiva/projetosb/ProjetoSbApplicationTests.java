a163c887dd640e20158a3897065fa22767e27ec3
Fernando Saraiva <fpsaraiva@gmail.com>
2021-07-23 23:11:49 -0300

    initial commit

---
+++ src/test/java/br/com/fpsaraiva/projetosb/ProjetoSbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fpsaraiva.projetosb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
