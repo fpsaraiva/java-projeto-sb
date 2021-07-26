7e596b711ca566c9988d2b3ad62dfd34f3d74115
Fernando Saraiva <fpsaraiva@gmail.com>
2021-07-25 21:04:15 -0300

    estudos iniciais webservices

---
+++ src/main/java/br/com/fpsaraiva/projetosb/controllers/ClienteController.java
@@ -0,0 +1,24 @@
+package br.com.fpsaraiva.projetosb.controllers;
+
+import br.com.fpsaraiva.projetosb.models.Cliente;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path="/clientes")
+public class ClienteController {
+
+    @GetMapping(path="/qualquer")
+    public Cliente obterCliente() {
+        return new Cliente(30, "Pedro", "000.000.000-00");
+    }
+
+    @GetMapping("/{id}")
+    public Cliente obterClientePorId1(@PathVariable int id) {
+        return new Cliente(id, "Maria", "111.111.000-00");
+    }
+
+    @GetMapping
+    public Cliente obterClientePorId2(@RequestParam(name="id") int id) {
+        return new Cliente(id, "João", "222.222.000-00");
+    }
+}
