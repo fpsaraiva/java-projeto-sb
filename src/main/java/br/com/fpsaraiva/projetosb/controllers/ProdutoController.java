30d317cf99128699f4e6d8553d907c65f0539b75
Fernando Saraiva <fpsaraiva@gmail.com>
2021-07-31 21:08:55 -0300

    feat: consulta todos produtos

--- src/main/java/br/com/fpsaraiva/projetosb/controllers/ProdutoController.java
+++ src/main/java/br/com/fpsaraiva/projetosb/controllers/ProdutoController.java
@@ -20,4 +20,9 @@ public class ProdutoController {
         produtoRepository.save(produto);
         return produto;
     }
+
+    @GetMapping
+    public Iterable<Produto> obterProdutos() {
+        return produtoRepository.findAll();
+    }
 }
